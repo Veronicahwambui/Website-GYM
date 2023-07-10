fbd6d7f2e7f96cdecd5081e4e6dc2c8a39fcbe47
veronicahWambui <njengaveronicahwambui@gmail.com>
2023-07-10 15:26:43 +0300

    made changes

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Plans from './components/Plans';
 import Testimonial from './components/Testimonials';
 import Join from './components/Join';
 import Footer from './components/Footer';
-import { Route,  BrowserRouter as Router, Routes } from 'react-router-dom'
-import Header from './components/Header/Header';
 function App() {
   return (
     <div className="App">
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './Header.css'
 import Logo from '../../assets/logo.png'
-import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
